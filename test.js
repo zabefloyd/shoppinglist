dabe7706dc2f8eb597ad2f8717ccda03515e7cea
Zabe <zabivirdee@gmail.com>
2016-05-18 05:08:27 +0200

    Add

---
+++ test.js
@@ -0,0 +1,22 @@
+
+function addListItem(){
+var text = $("#new-text").val();
+	
+$( "#todo" ).append('<li>'+text+' <button class="delete"> Delete </button></li> ');
+$("#new-text").val("");
+}
+
+function deleteListItem(){
+ $(this).parent().remove()
+ 
+}
+
+$(document).ready(function(){
+ 
+$("#add").on("click", addListItem);
+$(".delete").on("click",  deleteListItem);
+$(document).on("click", ".delete", deleteListItem);
+
+});
+
+
