d5201bcfc57dd3c9bee7ec22c83d3d24277e46f4
Zabe <zabivirdee@gmail.com>
2016-05-29 01:54:14 +0200

    msg

--- test.js
+++ test.js
@@ -2,7 +2,9 @@
 function addListItem(){
 var text = $("#new-text").val();
 	if (document.getElementById("new-text").value.length === 0){
-		alert("some text");
+		swal("Required field");
+		return false;
+
 	}
 	
 $( "#todo" ).append('<li>'+text+' <button class="delete"> Delete </button></li> ');
@@ -11,12 +13,11 @@ $("#new-text").val("");
 
 function deleteListItem(){
  $(this).parent().remove()
- 
 }
-
 $(document).ready(function(){
  
 $("#add").on("click", addListItem);
+
 $(".delete").on("click",  deleteListItem);
 $(document).on("click", ".delete", deleteListItem);
 
