45d33e9c16cd5aee3d6146555ff2fb32a149a63a
Zabe <zabivirdee@gmail.com>
2016-05-29 18:26:44 +0200

    msg

--- test.js
+++ test.js
@@ -1,27 +1,37 @@
 
 function addListItem(){
 var text = $("#new-text").val();
+//Not to add if input is empty
 	if (document.getElementById("new-text").value.length === 0){
 		document.getElementById("field-req").style.display = "block";
 		return false;
 		}
 		else{
-			document.getElementById("field-req").style.display = "none";
+		document.getElementById("field-req").style.display = "none";
 	}
-$( "#todo" ).append('<li>'+text+' <button class="delete"> Delete </button></li> ');
+$('#todo').append('<li>  &#x274E; '+text+' <button class="delete"> Delete </button></li>  ');
+
 $("#new-text").val("");
+ 
+
 }
 function deleteListItem(){
  $(this).parent().remove()
 }
 $(document).ready(function(){
- 
+ //add Item
 $("#add").on("click", addListItem);
+//Remove all the list
+$( "#removeall" ).click(function() {
+  $( "li" ).remove();
 
+});
+//delete Item
 $(".delete").on("click",  deleteListItem);
 $(document).on("click", ".delete", deleteListItem);
+
+//Add list by enter key code 13
 $(document).on('keypress', function (key) {
-    //keyCode == 13 is the ENTER key
     if (key.keyCode == 13) {
         addListItem();
     }
