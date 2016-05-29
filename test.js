a3ffaaf8d793b2ae80d225051c2b8cf2b36b8710
Zabe <zabivirdee@gmail.com>
2016-05-29 14:16:51 +0200

    msg

--- test.js
+++ test.js
@@ -2,9 +2,11 @@
 function addListItem(){
 var text = $("#new-text").val();
 	if (document.getElementById("new-text").value.length === 0){
-		swal("Required field");
+		document.getElementById("field-req").style.display = "block";
 		return false;
-
+		}
+		else{
+			document.getElementById("field-req").style.display = "none";
 	}
 	
 $( "#todo" ).append('<li>'+text+' <button class="delete"> Delete </button></li> ');
@@ -20,6 +22,12 @@ $("#add").on("click", addListItem);
 
 $(".delete").on("click",  deleteListItem);
 $(document).on("click", ".delete", deleteListItem);
+
+$(".sweet-overlay").click(function(){
+
+ $(".swal").hide();
+   event.preventDefault();
+});
 
 });
 
